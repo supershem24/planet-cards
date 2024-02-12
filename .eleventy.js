584385f44fa99cdbd617e2de8ee0aab656a6dcbc
supershem24 <107944240+supershem24@users.noreply.github.com>
2024-02-12 04:53:08 +0000

    Finished Card Grid, and created circles

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-  eleventyConfig.addShortcode("Card", function(planet){
+  eleventyConfig.addShortcode("Card", function(planet, distance){
     return `<section class="cardBackground" id=${planet}>
-      <div class=${planet}>
-        <p>Hi, I'm a planet</p>
-      </div>
+      <div class=${planet}></div>
+      <h2>${planet}</h2>
+      <p>${distance}</p>
     </section>`
   })
 };
